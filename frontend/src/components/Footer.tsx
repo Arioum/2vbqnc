9c911e24ba24069d0f88ddb50c447c872a6de75f
arioum <amoghrao121@gmail.com>
2024-07-26 23:27:30 +0530

    first commit

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,19 @@
+const Footer = () => {
+  return (
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500  py-10">
+      <div className="p-5 md:container mx-auto flex justify-between items-center">
+        <span className="flex flex-col">
+          <div className="text-3xl text-[#F9F9F8] font-bold tracking-tight">
+            Apna Hotel
+          </div>
+        </span>
+        <span className="text-[#F9F9F8] font-bold tracking-tight flex flex-col md:flex-row gap-2 md:gap-4">
+          <p className="cursor-pointer text-center">Privacy Policy</p>
+          <p className="cursor-pointer">Terms of service</p>
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ frontend/src/components/SignOutButton.tsx
@@ -0,0 +1,30 @@
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+
+const SignOutButton = () => {
+  const queryClient = useQueryClient();
+  const { showToast } = useAppContext();
+  const mutation = useMutation(apiClient.signOut, {
+    onSuccess: async () => {
+      await queryClient.invalidateQueries("validateToken"); // forcefully validate so that we don't need to refresh
+      showToast({ message: "Signed Out!", type: "SUCCESS" });
+    },
+    onError: (err: Error) => {
+      showToast({ message: err.message, type: "ERROR" });
+    },
+  });
+  const handleClick = () => {
+    mutation.mutate();
+  };
+  return (
+    <button
+      onClick={handleClick}
+      className="text-[#7263f3] px-3 font-bold bg-[#F9F9F8] hover:bg-gray-100"
+    >
+      Sign Out
+    </button>
+  );
+};
+
+export default SignOutButton;
